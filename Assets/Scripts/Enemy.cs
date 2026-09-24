b0aa5474e999d40d6c06d48f2ddca1be512013d1
SofiaS21 <49876490@est.ort.edu.ar>
2026-09-24 10:43:07 -0300

    Enemigos

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int damagePoints; //porque quiero acceder de otro script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public Enemy[] enemies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = FindObjectsOfType<Enemy>();
+        SetRandomValueToDamagePoints();
+        DeactivateEnemiesBelowSevenDamage(7);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDamagePointsTo(int newValue)
+    {
+        for(int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].damagePoints = newValue;
+        }
+
+    }
+
+    void SetRandomValueToDamagePoints()
+    {
+        for(int i = 0; i < enemies.Length; i++)
+        {
+
+            enemies[i].damagePoints = Random.Range(1, 11);
+        }
+    }
+
+    void DeactivateEnemiesBelowSevenDamage(int value)
+    {
+        for(int i = 0; i < enemies.Length; i++)
+        {
+            if(enemies[i].damagePoints < value)
+            {
+                enemies[i].gameObject.SetActive(false);
+            }
+            
+        }
+    }
+}
